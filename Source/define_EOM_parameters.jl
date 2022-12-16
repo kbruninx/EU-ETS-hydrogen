bc1c531ebf0630841f2c5bd547e9bd55d3b2868a
k.bruninx <k.bruninx@tudelft.nl>
2022-12-16 14:48:54 +0100

    minor correction

--- Source/define_EOM_parameters.jl
+++ Source/define_EOM_parameters.jl
@@ -4,7 +4,7 @@ function define_EOM_parameters!(EOM::Dict,data::Dict,ts::DataFrame,repr_days::Da
     EOM["GF"] = [sum(EOM["GF_YoY"][1:jy]) for jy=1:data["nyears"]]
 
     # timeseries
-    EOM["D_tot"] = [data["SF_load"]*ts[!,:LOAD][round(Int,data["nTimesteps"]*repr_days[!,:periods][jd]+jh)]/10^6 for jh=1:data["nTimesteps"], jd=1:data["nReprDays"]] # from MWh to TWh
+    EOM["D_tot"] = [ts[!,:LOAD][round(Int,data["nTimesteps"]*repr_days[!,:periods][jd]+jh)]/10^6 for jh=1:data["nTimesteps"], jd=1:data["nReprDays"]] # from MWh to TWh
     EOM["G_other"] = [ts[!,:OTHER][round(Int,data["nTimesteps"]*repr_days[!,:periods][jd]+jh)]/10^6 for jh=1:data["nTimesteps"], jd=1:data["nReprDays"]] # from MWh to TWh
     EOM["D"] = [(1+EOM["GF"][jy])*EOM["D_tot"][jh,jd] - EOM["G_other"][jh,jd] for jh=1:data["nTimesteps"], jd=1:data["nReprDays"], jy=1:data["nyears"]]
 
