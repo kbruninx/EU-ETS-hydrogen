82837ff6f54ec0ee1512f6fbcb54a378a91ec052
AlexanderHoogsteyn <hoogsteyna@gmail.com>
2023-03-10 16:13:11 +0100

    16 repr days

--- hpc/script.sh
+++ hpc/script.sh
@@ -3,13 +3,13 @@
 #SBATCH --job-name="EU-ETS-H2"
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=17
+#SBATCH --cpus-per-task=13
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --account=research-tpm-ess
-#SBATCH --array=2-6
+#SBATCH --array=1-4
 
 module load 2022r2
 module load julia
 
-srun julia --threads=17 MAIN.jl --start_scen $SLURM_ARRAY_TASK_ID --stop_scen $SLURM_ARRAY_TASK_ID --start_sens 17 --stop_sens 100 > run_$SLURM_ARRAY_TASK_ID.log
+srun julia --threads=13 MAIN.jl --start_scen $SLURM_ARRAY_TASK_ID --stop_scen $SLURM_ARRAY_TASK_ID --start_sens 17 --stop_sens 100 > run_$SLURM_ARRAY_TASK_ID.log
