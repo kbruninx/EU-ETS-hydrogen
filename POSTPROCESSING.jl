a3e020aa210e44fb587184cd388a251200d2c9b0
k.bruninx <k.bruninx@tudelft.nl>
2023-05-02 14:20:04 +0200

    May 2023

---
+++ POSTPROCESSING.jl
@@ -0,0 +1,37 @@
+# Packages 
+using DataFrames, CSV, YAML, DataStructures # dataprocessing
+
+# Constants
+const home_dir = @__DIR__
+
+# Functions 
+# Remove duplicate rows, keep last entry, modified from: https://discourse.julialang.org/t/delete-duplicate-rows-in-a-dataframe/65487/10
+function dropduplicates(df, cols; keep = :first) 
+    keep in [:first, :last] || error("keep parameter should be :first or :last")
+    combine(groupby(df, cols)) do sdf
+        if nrow(sdf) == 1 
+            DataFrame(
+                sdf
+            )
+        else
+            DataFrame(
+              filter(
+                r->rownumber(r)==(keep == :first ? 1 : nrow(sdf)), 
+                eachrow(sdf)
+              )
+            )
+        end
+    end
+end
+
+# Read overview results file and clean up 
+df = CSV.read(joinpath(home_dir,"overview_results_8_repr_days.csv"),delim=";",DataFrame)
+df_filtered = dropduplicates!(df, [:scen_number, :sensitivity], keep = :last)
+sort!(df_filtered, [:scen_number])
+CSV.write(joinpath(home_dir,"overview_results_8_repr_days_CLEAN.csv"),delim=";",df_filtered)
+
+
+# Limit on number of iterations hit
+df_walltime_hit = filter!(row -> row.n_iter == 10000, df_filtered)
+CSV.write(joinpath(home_dir,"overview_results_8_repr_days_ITERATION_LIMIT.csv"),delim=";",df_filtered)
+
--- Source/update_supply.jl
+++ Source/update_supply.jl
@@ -5,104 +5,94 @@ function update_supply!(e::Array,ETS::Dict,data::Dict)
     # MSR will be shifted by 1 yeare (i.e., MSR[y,12] is the MSR at the end of year y-1)
 
     if data["MSR"] == "YES" #   2021-2023 based on 2018 rules, 2024 - end based on 2023 rules
-
-        # MSR 2018 
-        for y = 1:3
+        for y = 1:data["nyears"]
             for m = 1:12
-                if m <= 8 # For the first 8 months, intake/outflow MSR depends on the TNAC in y-2
-                    if ETS["TNAC"][y] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
-                        if min(ETS["CAP"][y],ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]) > ETS["X_MSR_MAX_POS"][y]*data["TNAC_MAX"] # only put EUAs in MSR if total exceeds 100/200 million
-                                ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                if y <= 3 # MSR2018 
+                    if m <= 8 # For the first 8 months, intake/outflow MSR depends on the TNAC in y-2
+                        if ETS["TNAC"][y] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
+                            if min(ETS["CAP"][y],ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]) > ETS["X_MSR_MAX_POS"][y]*data["TNAC_MAX"] # only put EUAs in MSR if total exceeds 100/200 million
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                            else
+                                    ETS["X_MSR"][y,m] = 0
+                            end
+                        elseif ETS["TNAC"][y] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
+                                    ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y,12]/8) # outflow limited to what is in MSR
                         else
-                                ETS["X_MSR"][y,m] = 0
+                                    ETS["X_MSR"][y,m] = 0
                         end
-                    elseif ETS["TNAC"][y] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
+                    else # For the last 4 months, intake/outflow MSR depends on the TNAC in y-1
+                        if ETS["TNAC"][y+1] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
+                            if min(ETS["CAP"][y],ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1])> ETS["X_MSR_MAX_POS"][y]*data["TNAC_MAX"] # only put EUAs in MSR if total exceeds 200/100 million
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                            else
+                                    ETS["X_MSR"][y,m] = 0
+                            end
+                        elseif ETS["TNAC"][y+1] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
+                                    ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y+1,8]/4) # outflow limited to what is in MSR
+                        else
+                                    ETS["X_MSR"][y,m] = 0
+                        end
+                    end
+
+                    # Adapt MSR with backloaded/non-allocated allowances
+                    if m == 1
+                            ETS["MSR"][y+1,m] = ETS["MSR"][y,12]+ETS["DELTA"][y]+ETS["X_MSR"][y,1]
+                    else
+                            ETS["MSR"][y+1,m] = ETS["MSR"][y+1,m-1]+ETS["X_MSR"][y,m]
+                    end
+                    
+                    # Cancellation enforced as of 2023
+                    if y >= 3
+                        if ETS["MSR"][y+1,m] > 0.57*ETS["CAP"][y-1] 
+                            ETS["C"][y,m] =  ETS["MSR"][y+1,m]-0.57*ETS["CAP"][y-1]
+                            ETS["MSR"][y+1,m] = 0.57*ETS["CAP"][y-1]
+                        end
+                    else
+                            ETS["C"][y,m] = 0
+                    end
+                else # MSR 2023
+                    for m = 1:12
+                        if m <= 8 # For the first 8 months, intake/outflow MSR depends on the TNAC in y-2
+                            if ETS["TNAC"][y] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
+                                if ETS["TNAC"][y] <= data["TNAC_THRESHOLD"]  # between new threshold and maximum 
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,(ETS["TNAC"][y]-data["TNAC_MAX"])/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                                else # regular intake rate 
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                                end
+                            elseif ETS["TNAC"][y] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
                                 ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y,12]/8) # outflow limited to what is in MSR
-                    else
+                            else
                                 ETS["X_MSR"][y,m] = 0
-                    end
-                else # For the last 4 months, intake/outflow MSR depends on the TNAC in y-1
-                    if ETS["TNAC"][y+1] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
-                        if min(ETS["CAP"][y],ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1])> ETS["X_MSR_MAX_POS"][y]*data["TNAC_MAX"] # only put EUAs in MSR if total exceeds 200/100 million
-                                ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
-                        else
-                                ETS["X_MSR"][y,m] = 0
-                        end
-                    elseif ETS["TNAC"][y+1] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
+                            end
+                        else # For the last 4 months, intake/outflow MSR depends on the TNAC in y-1
+                            if ETS["TNAC"][y+1] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
+                                if ETS["TNAC"][y+1] <= data["TNAC_THRESHOLD"] # between new threshold and maximum 
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,(ETS["TNAC"][y+1]-data["TNAC_MAX"])/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                                else # regular intake rate  
+                                    ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                                end
+                            elseif ETS["TNAC"][y+1] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
                                 ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y+1,8]/4) # outflow limited to what is in MSR
-                    else
+                            else
                                 ETS["X_MSR"][y,m] = 0
-                    end
-                end
-
-                # Adapt MSR with backloaded/non-allocated allowances
-                if m == 1
-                        ETS["MSR"][y+1,m] = ETS["MSR"][y,12]+ETS["DELTA"][y]+ETS["X_MSR"][y,1]
-                else
-                        ETS["MSR"][y+1,m] = ETS["MSR"][y+1,m-1]+ETS["X_MSR"][y,m]
-                end
-
-                # Cancellation enforced as of 2023
-                if y >= 3
-                    if ETS["MSR"][y+1,m] > 0.57*ETS["CAP"][y-1] 
-                        ETS["C"][y,m] =  ETS["MSR"][y+1,m]-0.57*ETS["CAP"][y-1]
-                        ETS["MSR"][y+1,m] = 0.57*ETS["CAP"][y-1]
-                    end
-                else
-                        ETS["C"][y,m] = 0
-                end
-            end
-
-            # Corrected supply of EUAS 
-            ETS["S"][y]  = maximum([0,ETS["CAP"][y] - sum(ETS["X_MSR"][y,1:12]) + ETS["Δs"][y]]) 
-          
-            # TNAC
-            # ETS["TNAC"][y] = sum(ETS["CAP"][1:y]) + sum(ETS["Δs"][1:y]) + sum(ETS["DELTA"][1:y]) - sum(e[1:y]) - sum(ETS["C"][1:y,:]) - ETS["MSR"][y,12]
-            ETS["TNAC"][y+2] = ETS["TNAC"][y+1] + ETS["CAP"][y] + ETS["Δs"][y] + ETS["DELTA"][y] - e[y] - sum(ETS["C"][y,:]) - ETS["MSR"][y+1,12] # recursive to account for "initial" state of TNAC (TNAC is added to supply in preceding year to make EUAs avaialble to actors, otherwise double counting)
-        end
-
-        # 2023 MSR rules - apply from 2024 onwards 
-        for y = 4:data["nyears"]
-            for m = 1:12
-                if m <= 8 # For the first 8 months, intake/outflow MSR depends on the TNAC in y-2
-                    if ETS["TNAC"][y] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
-                        if ETS["TNAC"][y] <= data["TNAC_THRESHOLD"]  # between new threshold and maximum 
-                            ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,(ETS["TNAC"][y]-data["TNAC_MAX"])/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
-                        else # regular intake rate 
-                            ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/8,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+                            end
                         end
-                    elseif ETS["TNAC"][y] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
-                        ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y,12]/8) # outflow limited to what is in MSR
-                    else
-                        ETS["X_MSR"][y,m] = 0
-                    end
-                else # For the last 4 months, intake/outflow MSR depends on the TNAC in y-1
-                    if ETS["TNAC"][y+1] >= data["TNAC_MAX"] # If exceeds TNAC_MAX - inflow
-                        if ETS["TNAC"][y+1] <= data["TNAC_THRESHOLD"] # between new threshold and maximum 
-                            ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,(ETS["TNAC"][y+1]-data["TNAC_MAX"])/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
-                        else # regular intake rate  
-                            ETS["X_MSR"][y,m] = min(ETS["CAP"][y]/4,ETS["X_MSR_MAX_POS"][y]*ETS["TNAC"][y+1]/12) # one cannot put more in the MSR than what's still left to be auctioned/allocated
+        
+                        # Adapt MSR with backloaded/non-allocated allowances
+                        if m == 1
+                                ETS["MSR"][y+1,m] = ETS["MSR"][y,12]+ETS["DELTA"][y]+ETS["X_MSR"][y,1]
+                        else
+                                ETS["MSR"][y+1,m] = ETS["MSR"][y+1,m-1]+ETS["X_MSR"][y,m]
                         end
-                    elseif ETS["TNAC"][y+1] <= data["TNAC_MIN"] # if below TNAC_MIN - outflow 
-                        ETS["X_MSR"][y,m] = -min(ETS["X_MSR_MAX_NEG"][y]/12,ETS["MSR"][y+1,8]/4) # outflow limited to what is in MSR
-                    else
-                        ETS["X_MSR"][y,m] = 0
+        
+                        # Cancellation enforced as of 2023
+                        if ETS["MSR"][y+1,m] > data["TNAC_MIN"] && y >= 3  
+                                ETS["C"][y,m] =  ETS["MSR"][y+1,m]-data["TNAC_MIN"]
+                                ETS["MSR"][y+1,m] = data["TNAC_MIN"]
+                        else
+                                ETS["C"][y,m] = 0
+                        end
                     end
-                end
-
-                # Adapt MSR with backloaded/non-allocated allowances
-                if m == 1
-                        ETS["MSR"][y+1,m] = ETS["MSR"][y,12]+ETS["DELTA"][y]+ETS["X_MSR"][y,1]
-                else
-                        ETS["MSR"][y+1,m] = ETS["MSR"][y+1,m-1]+ETS["X_MSR"][y,m]
-                end
-
-                # Cancellation enforced as of 2023
-                if ETS["MSR"][y+1,m] > data["TNAC_MIN"] && y >= 3  
-                        ETS["C"][y,m] =  ETS["MSR"][y+1,m]-data["TNAC_MIN"]
-                        ETS["MSR"][y+1,m] = data["TNAC_MIN"]
-                else
-                        ETS["C"][y,m] = 0
                 end
             end
 
@@ -110,11 +100,9 @@ function update_supply!(e::Array,ETS::Dict,data::Dict)
             ETS["S"][y]  = maximum([0,ETS["CAP"][y] - sum(ETS["X_MSR"][y,1:12]) + ETS["Δs"][y]]) 
             
             # TNAC
-            # ETS["TNAC"][y] = sum(ETS["S"][1:y]) - sum(e[1:y]) 
             ETS["TNAC"][y+2] =  ETS["TNAC"][y+1] + ETS["S"][y] - e[y] # recursive to account for "initial" state of TNAC (TNAC is added to supply in preceding year to make EUAs avaialble to actors, otherwise double counting)
         end      
-
-    elseif data["MSR"] == "NO" #   # No MSR, supply equals cap except in 2021
+    elseif data["MSR"] == "NO"  # No MSR, supply equals cap except in 2021
         ETS["S"] = copy(ETS["CAP"])
         for y = 1:data["nyears"]
             ETS["TNAC"][y+2] =  ETS["TNAC"][y+1] + ETS["S"][y] - e[y]
